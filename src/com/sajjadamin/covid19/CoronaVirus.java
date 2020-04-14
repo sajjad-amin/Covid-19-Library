f219a7d504e764256870d6be39bfde81815acf72
sajjad-amin <sayem.1998.bd@gmail.com>
2020-04-14 23:11:29 +0600

    rebuild project using core functions of Jsoup

---
+++ src/com/sajjadamin/covid19/CoronaVirus.java
@@ -0,0 +1,78 @@
+package com.sajjadamin.covid19;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+/**
+ *
+ * @author Sajjad Amin
+ */
+public class CoronaVirus {
+    private ArrayList<DataList> list = new ArrayList<>();
+    public String getJsonStatistics() throws IOException{
+        String html = Jsoup.connect("https://worldometers.info/coronavirus/").get().html();
+        Document document = Jsoup.parse(html);
+        Element tbody = document.getElementById("main_table_countries_today").getElementsByTag("tbody").first();
+        JSONArray jarray = new JSONArray();
+        for (int i = 0; i < tbody.getElementsByTag("tr").size(); i++) {
+            Element tr = tbody.getElementsByTag("tr").get(i);
+            String country,total_cases,new_cases,total_deaths,new_deaths,total_recovered,active_cases,serious_cases;
+                country = tr.getElementsByTag("td").get(0).text();
+                total_cases = tr.getElementsByTag("td").get(1).text();
+                new_cases = tr.getElementsByTag("td").get(2).text();
+                total_deaths = tr.getElementsByTag("td").get(3).text();
+                new_deaths = tr.getElementsByTag("td").get(4).text();
+                total_recovered = tr.getElementsByTag("td").get(5).text();
+                active_cases = tr.getElementsByTag("td").get(6).text();
+                serious_cases = tr.getElementsByTag("td").get(7).text();
+                if(total_cases.equals("")) total_cases = "0";
+                if(new_cases.equals("")) new_cases = "0";
+                if(total_deaths.equals("")) total_deaths = "0";
+                if(new_deaths.equals("")) new_deaths = "0";
+                if(total_recovered.equals("")) total_recovered = "0";
+                if(active_cases.equals("")) active_cases = "0";
+                if(serious_cases.equals("")) serious_cases = "0";
+                JSONObject jobject = new JSONObject();
+                jobject.put("country", country);
+                jobject.put("total_cases", total_cases);
+                jobject.put("new_cases", new_cases.replace("+", ""));
+                jobject.put("total_deaths", total_deaths);
+                jobject.put("new_deaths", new_deaths.replace("+", ""));
+                jobject.put("total_recovered", total_recovered);
+                jobject.put("active_cases", active_cases);
+                jobject.put("serious_cases", serious_cases);
+                jarray.put(jobject);
+        }
+        return jarray.toString();
+    }
+    public ArrayList<DataList> getDataList() throws IOException{
+        String html = Jsoup.connect("https://worldometers.info/coronavirus/").get().html();
+        Document document = Jsoup.parse(html);
+        Element tbody = document.getElementById("main_table_countries_today").getElementsByTag("tbody").first();
+        for (int i = 0; i < tbody.getElementsByTag("tr").size(); i++) {
+            Element tr = tbody.getElementsByTag("tr").get(i);
+            String country,total_cases,new_cases,total_deaths,new_deaths,total_recovered,active_cases,serious_cases;
+                country = tr.getElementsByTag("td").get(0).text();
+                total_cases = tr.getElementsByTag("td").get(1).text();
+                new_cases = tr.getElementsByTag("td").get(2).text();
+                total_deaths = tr.getElementsByTag("td").get(3).text();
+                new_deaths = tr.getElementsByTag("td").get(4).text();
+                total_recovered = tr.getElementsByTag("td").get(5).text();
+                active_cases = tr.getElementsByTag("td").get(6).text();
+                serious_cases = tr.getElementsByTag("td").get(7).text();
+                if(total_cases.equals("")) total_cases = "0";
+                if(new_cases.equals("")) new_cases = "0";
+                if(total_deaths.equals("")) total_deaths = "0";
+                if(new_deaths.equals("")) new_deaths = "0";
+                if(total_recovered.equals("")) total_recovered = "0";
+                if(active_cases.equals("")) active_cases = "0";
+                if(serious_cases.equals("")) serious_cases = "0";
+                list.add(new DataList(country,total_cases,new_cases.replace("+", ""),total_deaths,new_deaths.replace("+", ""),total_recovered,active_cases,serious_cases));
+        }
+        return list;
+    }
+}
---
+++ src/com/sajjadamin/covid19/DataList.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sajjadamin.covid19;
+/**
+ *
+ * @author sayem
+ */
+public class DataList {
+    String countryName,totalCases,newCases,totalDeaths,newDeaths,totalRecovered,activeCases,seriousCases;
+
+    public DataList() {
+    }
+
+    public DataList(String countryName, String totalCases, String newCases, String totalDeaths, String newDeaths, String totalRecovered, String activeCases, String seriousCases) {
+        this.countryName = countryName;
+        this.totalCases = totalCases;
+        this.newCases = newCases;
+        this.totalDeaths = totalDeaths;
+        this.newDeaths = newDeaths;
+        this.totalRecovered = totalRecovered;
+        this.activeCases = activeCases;
+        this.seriousCases = seriousCases;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getTotalCases() {
+        return totalCases;
+    }
+
+    public String getNewCases() {
+        return newCases;
+    }
+
+    public String getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    public String getNewDeaths() {
+        return newDeaths;
+    }
+
+    public String getTotalRecovered() {
+        return totalRecovered;
+    }
+
+    public String getActiveCases() {
+        return activeCases;
+    }
+
+    public String getSeriousCases() {
+        return seriousCases;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setTotalCases(String totalCases) {
+        this.totalCases = totalCases;
+    }
+
+    public void setNewCases(String newCases) {
+        this.newCases = newCases;
+    }
+
+    public void setTotalDeaths(String totalDeaths) {
+        this.totalDeaths = totalDeaths;
+    }
+
+    public void setNewDeaths(String newDeaths) {
+        this.newDeaths = newDeaths;
+    }
+
+    public void setTotalRecovered(String totalRecovered) {
+        this.totalRecovered = totalRecovered;
+    }
+
+    public void setActiveCases(String activeCases) {
+        this.activeCases = activeCases;
+    }
+
+    public void setSeriousCases(String seriousCases) {
+        this.seriousCases = seriousCases;
+    }
+    
+}
